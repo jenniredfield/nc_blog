a574d9ebb949e581682ee0940dc00968fad1d00a
Megan Field <megan.field@hotmail.co.uk>
2018-01-16 12:11:58 +0000

    pagination for all posts and individual authors

--- src/Authors.js
+++ src/Authors.js
@@ -5,15 +5,16 @@ class Authors extends React.Component {
     state = {
         name: null,
         posts: [],
+        page: null,
     }
 
-    
-    componentDidMount () {
-        const {name} = this.props.match.params
+
+    componentDidMount() {
+        const { name } = this.props.match.params
         this.fetchPostsByAuthor(name);
     }
-   
-    componentWillReceiveProps (nextProps) {
+
+    componentWillReceiveProps(nextProps) {
         const oldName = this.props.match.params.name
         const newName = nextProps.match.params.name
         if (oldName !== newName) {
@@ -24,39 +25,69 @@ class Authors extends React.Component {
 
     fetchPostsByAuthor = (name) => {
         return fetch(`https://northcoders-sprints-api.now.sh/api/blog/authors/${name}`)
-          .then((resBuffer) => resBuffer.json())
-          .then((res) => {
-            this.setState({ 
-              posts:  res.posts,
-              name:  name,
-            });
-          
-          })
-          .catch(console.log);
-      };   
+            .then((resBuffer) => resBuffer.json())
+            .then((res) => {
+                this.setState({
+                    posts: res.posts,
+                    name: name,
+                    page: res.page,
+                });
 
-       
-       
+            })
+            .catch(console.log);
+    };
 
-      render() {
-            return (
+    fetchNextPost = (event) => {
+        console.log(this.state.name)
+        let currentPage = this.state.page;
+        let nextPage = 0;
+        if (event.target.value === 'next') nextPage = currentPage + 1;
+        if (event.target.value === 'previous') nextPage = currentPage - 1;
+        console.log(nextPage, 'nextPage')
+        console.log(currentPage, 'currentPage')
+        if (nextPage < 0) nextPage = 0;
+        if (nextPage > 9) nextPage = 9;
+
+        return fetch(`https://northcoders-sprints-api.now.sh/api/blog/authors/${this.state.name}?page=${nextPage}`)
+            .then((resBuffer) => resBuffer.json())
+            .then((res) => {
+                console.log(res)
+                this.setState({
+                    page: res.page,
+                    posts: res.posts,
+                });
+
+            })
+            .catch(console.log);
+
+
+    }
+
+
+
+
+    render() {
+        return (
             <div className="postByAuthor-wrapper">
                 <h2>{this.state.name}</h2>
 
-             {  this.state.posts.map((post, i) => {
-                   return (
-                   <div className="author-post-div" key={i}>
-                        <h3>{post.title}</h3>
-                        <p className="author-posts">{post.body}</p> 
-                   </div> 
-                   )
-                   }) }
+                {this.state.posts.map((post, i) => {
+                    return (
+                        <div className="author-post-div" key={i}>
+                            <h3>{post.title}</h3>
+                            <p className="author-posts">{post.body}</p>
+                        </div>
+                    )
+                })}
+                <div>
+                    <button type="submit" value="previous" onClick={this.fetchNextPost} disabled={this.state.page === 0 ? true : false}>Previous</button>
+                    <button type="submit" value="next" onClick={this.fetchNextPost} disabled={this.state.page === 9 ? true : false}>Next</button>
+                </div>
+            </div>
 
-               </div>
 
-
-            );
-      }
+        );
+    }
 }
 
 export default Authors;
--- src/LatestPosts.js
+++ src/LatestPosts.js
@@ -1,62 +1,83 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class LatestPosts extends React.Component {
 
     state = {
         posts: [],
-
+        page: null,
     }
 
-    componentDidMount () {
+    componentDidMount() {
         this.fetchPosts();
-      }
+    }
 
 
     fetchPosts = () => {
-
-
-            return fetch(`https://northcoders-sprints-api.now.sh/api/blog/posts`)
-              .then((resBuffer) => resBuffer.json())
-              .then((res) => {
+        return fetch(`https://northcoders-sprints-api.now.sh/api/blog/posts`)
+            .then((resBuffer) => resBuffer.json())
+            .then((res) => {
                 console.log(res)
-                this.setState({ 
-                
-                  posts:  res.posts,
+                this.setState({
+                    page: res.page,
+                    posts: res.posts,
                 });
-              
-              })
-              .catch(console.log);
-          };
+
+            })
+            .catch(console.log);
+    };
+
+    fetchNextPost = (event) => {
+        let currentPage = this.state.page;
+        let nextPage = 0;
+        if (event.target.value === 'next') nextPage = currentPage + 1;
+        if (event.target.value === 'previous') nextPage = currentPage - 1;
+        if (nextPage < 0) nextPage = 0;
+        if (nextPage > 9) nextPage = 9;
     
-    
+        return fetch(`https://northcoders-sprints-api.now.sh/api/blog/posts?page=${nextPage}`)
+            .then((resBuffer) => resBuffer.json())
+            .then((res) => {
+                console.log(res)
+                this.setState({
+                    page: res.page,
+                    posts: res.posts,
+                });
+
+            })
+            .catch(console.log);
 
 
-     render ()  {
-        
-           return <div className="posts-wrapper">
+    }
 
-           { this.state.posts.map(function(post){
+    render() {
+
+        return <div className="posts-wrapper">
+
+            {this.state.posts.map(function (post) {
                 return (
 
-                    <Link  to={`/posts/${post.id}`}>
-                    <div className="post-snippet">
-                        <h2>{post.author}</h2>
-                        <p className="description">{post.title}</p>
-                    </div>
-                     </Link>
+                    <Link to={`/posts/${post.id}`}>
+                        <div className="post-snippet">
+                            <h2>{post.author}</h2>
+                            <p className="description">{post.title}</p>
+                        </div>
+                    </Link>
 
                 )
 
-           })
+            })
 
-           }
-
-
-    
+            }
+            <div>
+                <button type="submit" value="previous" onClick={this.fetchNextPost} disabled={this.state.page === 0 ? true : false}>Previous</button>
+                <button type="submit" value="next" onClick={this.fetchNextPost} disabled={this.state.page === 9 ? true : false}>Next</button>
             </div>
 
-        }
+
+        </div>
+
+    }
 
 }
 
